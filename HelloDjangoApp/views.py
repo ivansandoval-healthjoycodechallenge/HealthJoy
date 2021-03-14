58d681be8301ca75ee98b96dd11d3bcfae802439
Ivan Sandoval <mellamodrago@live.com>
2021-03-13 20:22:53 -0500

    notes

--- HelloDjangoApp/views.py
+++ HelloDjangoApp/views.py
@@ -12,7 +12,14 @@ from django.contrib import messages
 def index(request):
     if request.method == 'POST':
         form = GitHubCreds(request.POST)
-        if form.is_valid():            
+        if form.is_valid():     
+            
+            # Please note..
+            # Ideally I would make an API call and not have the below GitHub logic coupled to this button event
+                #reusability - call the 'FORK' endpoint from anywhere, not just this form...
+                #security - adding OAuth authentication/authorization for the API/endpoint creates another layer of security
+            # Not familiar enough with Python/Django, would take significant amount of time to build out a legitimate solution for this (front facing app and api with oauth)
+            
            try:
             # Github username
             token = request.POST.get('token')
@@ -21,19 +28,18 @@ def index(request):
             gg = Github(token)
             github_user = gg.get_user()
         
-            # this applications repo PAT 
-            # ideally store secure... 
+            #ideally store in app settings, config file of some sort...                   
             g = Github('bc54d96e80ae7a915c0666683f976ddee4cc009b')     
             repo = g.get_repo("ivansandoval-healthjoycodechallenge/HealthJoy")            
 
-             # fork the repo to the users account
+            # fork the repo to the users account
             myfork = github_user.create_fork(repo)  
                    
             #give the user a success message
             messages.success(request, 'Fork successful!')
 
            #reasonable to catch separately and do different things (timeouts, too many requests, etc.)
-           #for the sake of this im just going to catch all
+           #for the sake of this im just going to catch all under 'Exception'
            except Exception as e:
                 messages.error(request, e)
            
